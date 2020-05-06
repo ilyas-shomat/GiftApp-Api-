b42bf9e7c61e7d1d068b77b4d95ec2b403795fa1
ilyasshomat <ilyas.shomat@gmail.com>
2020-05-06 19:15:47 +0600

    Get liked wish list and like some wish is done

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+import os
 from src.app import create_app
 
 if __name__ == '__main__':
   env_name = 'development'
   app = create_app(env_name)
 
-  app.run()
+  app.run(host='0.0.0.0', port=8080)

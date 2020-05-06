da9c43e6461200132754b8c06851f5f7a85eb094
ilyasshomat <ilyas.shomat@gmail.com>
2020-05-06 19:18:24 +0600

    Deployein on heroku

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = 'sisoft9&o4!$-<#ilyas&&iliyas'
-    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:8648@localhost:5432/gift_app'
+    SQLALCHEMY_DATABASE_URI = 'postgres://nlcdeuiwwkpdqo:6d8752cce8036185f550b2abc2784e364f3ffee0898d495e2dc314d33df1f1d8@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d8mdouc33b6jq5'
 #   postgres://name:password@host:port/db_name
 
 class Production(object):
@@ -16,7 +16,7 @@ class Production(object):
     DEBUG = False
     TESTING = False
     JWT_SECRET_KEY = 'sisoft9&o4!$-<#ilyas&&iliyas'
-    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:8648@localhost:5432/gift_app'
+    SQLALCHEMY_DATABASE_URI = 'postgres://nlcdeuiwwkpdqo:6d8752cce8036185f550b2abc2784e364f3ffee0898d495e2dc314d33df1f1d8@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d8mdouc33b6jq5'
 
 app_config = {
     'development': Development,
